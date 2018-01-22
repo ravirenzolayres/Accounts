dc15c61d389ea02210d9f9e5ea08199c598914a6
Ravi Renz Olayres <renzolayres88@gmail.com>
2018-01-22 14:30:29 +0800

    Added EmployeeID on Web Authentication

--- AccountsWebAuthentication/Controllers/BaseAccountsController.cs
+++ AccountsWebAuthentication/Controllers/BaseAccountsController.cs
@@ -1,5 +1,6 @@
-﻿    using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using AccountsFunction;
+using AndersonCRMFunction;
 using AccountsWebAuthentication.Helper;
 
 namespace AccountsWebAuthentication.Controllers
@@ -7,10 +8,12 @@ namespace AccountsWebAuthentication.Controllers
     public class BaseAccountsController : Controller
     {
         private IFUser _iFUser;
+        private IFEmployee _iFEmployee;
 
         public BaseAccountsController()
         {
             _iFUser = new FUser();
+            _iFEmployee = new FEmployee();
         }
 
         protected string Username
@@ -28,6 +31,16 @@ namespace AccountsWebAuthentication.Controllers
                 var user = _iFUser.Read(Username);
                 int UserID = user?.UserId ?? 0;
                 return UserID;
+            }
+        }
+
+        protected int EmployeeId
+        {
+            get
+            {
+                var employee = _iFEmployee.Read(EmployeeId);
+                int EmployeeID = employee?.EmployeeId ?? 0;
+                return EmployeeID;
             }
         }
 
